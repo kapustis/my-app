64e1ff0804ca6160d2d99cb3cce46caa7e8ccaa9
kapustis <kapustis73@gmail.com>
2020-01-13 10:10:46 +0200

    no item

--- src/App.js
+++ src/App.js
@@ -10,28 +10,34 @@ class App extends React.Component {
             {id: 2, name: 'Toyota', model: 'AE86 Apex'},
             {id: 3, name: 'BMW', model: 'M3 E30'},
         ],
+        showCar: false,
         headText: 'Редактировать'
     };
     changeHeadText = (newText) => {
         this.setState({headText: newText})
     };
+    toggleShowCars = () =>{
+        this.setState({showCar: !this.state.showCar})
+    };
 
-    carItem() {
+    carItem =() => {
         return this.state.cars.map((car,index) =>
             <Car key={car.id} name={car.name} model={car.model} onChangeText={() => this.changeHeadText(car.name)}>
                 <p>Индекс елемента :{index} и id:{car.id}</p>
             </Car>
         );
-    }
+    };
 
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <h1>{this.state.headText}</h1>
+                    <button onClick={this.toggleShowCars}>Toggle cars</button>
                 </header>
                 <div className='content'>
-                    {this.carItem()}
+
+                    { this.state.showCar ? this.carItem() : <p>no item</p>}
                     {/*{this.state.cars.map((car, index) => {*/}
                     {/*        return (*/}
                     {/*            <Car key={car.id} name={car.name} model={car.model} onChangeText={() => this.changeHeadText(car.name)}>*/}
